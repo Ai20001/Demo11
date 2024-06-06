c75d571057ca6551bbd74147dcc236dabd9a2b27
✩ĐɆⱠ₮₳✩ <155818429+Ai20001@users.noreply.github.com>
2024-06-06 15:25:28 +0300

    Delete loading.js

--- loading.js
+++
@@ -1,8 +0,0 @@
-function showLoader() {
-    var loaderImg = $('<img>').attr('src', '/static/images/loading.png').addClass('loader-img');
-    $('#loader-container').append(loaderImg);
-}
-
-function hideLoader() {
-    $('#loader-container').empty();
-}
